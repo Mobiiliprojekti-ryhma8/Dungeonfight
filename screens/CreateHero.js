fe7199a2da0ee4086c6c1920edc867935f52e98a
taunoo01 <tatu.malinen1@gmail.com>
2024-11-11 18:44:11 +0200

    databaseen onnistuu lisäämään sankarihahmon

--- screens/CreateHero.js
+++ screens/CreateHero.js
@@ -1,14 +1,67 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, Text, Button, TextInput, Alert } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import React, { useState } from 'react';
+import { addHero } from '../firebase/Config';
 
-export default function CreateHero({navigation}) {
+const CreateCharacter = () => {
+  const [name, setName] = useState('');
+  const [heroClass, setHeroClass] = useState('');
+
+  const handleCreateHero = async () => {
+    let health = 0;
+    let damage = 0;
+
+    if (heroClass === 'warrior') {
+      health = 25;
+      damage = 3;
+    } else if (heroClass === 'wizard') {
+      health = 20;
+      damage = 5;
+    }
+
+    try {
+      await addHero(name, heroClass, 0, health, damage);
+      Alert.alert(`${heroClass} has been created!`);
+    } catch (error) {
+      console.error('Error creating hero:', error);
+      Alert.alert('Failed to create hero');
+    }
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Create Hero</Text>
-      <Button
-        title='Create Hero'
-        onPress={() => navigation.navigate('StartDungeon')}
+    <View style={{ padding: 20 }}>
+      <Text style={{ fontSize: 18, marginBottom: 10 }}>Create a New Hero</Text>
+
+      {}
+      <Text>Name:</Text>
+      <TextInput
+        style={{
+          borderWidth: 1,
+          borderColor: 'gray',
+          padding: 10,
+          marginBottom: 15,
+        }}
+        placeholder="Name your hero"
+        value={name}
+        onChangeText={setName}
       />
+
+      {}
+      <Text>Choose class:</Text>
+      <Picker
+        selectedValue={heroClass}
+        onValueChange={(itemValue) => setHeroClass(itemValue)}
+        style={{ marginBottom: 15 }}
+      >
+        <Picker.Item label="Select Class" value="" />
+        <Picker.Item label="Warrior" value="warrior" />
+        <Picker.Item label="Wizard" value="wizard" />
+      </Picker>
+
+      {}
+      <Button title="Create Hero" onPress={handleCreateHero} />
     </View>
-  )
-}
+  );
+};
+
+export default CreateCharacter;
